3c192b69526f10b48a5b653e5297dba6e96ba978
Jean Monteiro <jeanmontt@hotmail.com>
2020-06-30 14:50:01 -0300

    Add challenge 3-1

---
+++ fase_02/03_inciando_back_end/desafio_03-1_primeiro-servidor/public/scripts/scripts.js
@@ -0,0 +1,28 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+const modal = document.querySelector('#modal');
+const classes = modal.classList;
+
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        const pageID = card.getAttribute("id");
+        modalOverlay.classList.add('active');
+        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${pageID}`;
+    })
+};
+
+document.querySelector('.close-modal').addEventListener("click", function() {
+    modalOverlay.classList.remove('active');
+    modal.classList.remove('maximize');
+    modalOverlay.querySelector('iframe').src = "";
+});
+
+document.querySelector('.maximize-modal').addEventListener('click', function(){
+    let result = classes.toggle("maximize");
+
+    if (result) {
+        modal.classList.add('maximize');
+    } else {
+        modal.classList.remove('maximize');
+    }
+})
---
+++ fase_02/03_inciando_back_end/desafio_03-1_primeiro-servidor/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const server = express();
+
+server.use(express.static("public"));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server,
+    noCache: true
+});
+
+server.get("/", function(req, res) {
+    return res.render("index");
+});
+
+server.get("/courses", function(req, res) {
+    return res.render("courses");
+});
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+server.listen(5000);
