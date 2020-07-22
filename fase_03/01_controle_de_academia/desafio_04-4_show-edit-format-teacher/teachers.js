8bf7f193fd2aac93d5945a75f4c0f63fd1322098
Jean Monteiro <jeanmontt@hotmail.com>
2020-07-21 22:06:04 -0300

    Add challenge 4-4

---
+++ fase_03/01_controle_de_academia/desafio_04-4_show-edit-format-teacher/teachers.js
@@ -0,0 +1,88 @@
+const fs = require('fs');   //requisição do file system
+const data = require('./data.json'); //requisição dos dados
+const { age, date } = require('./utils'); //desestruturando e requerendo as funções age e date
+const Intl = require('intl');  //requerendo o Intl para formatação
+
+// ============= POST =============
+exports.post = function (req, res) {
+    //estrutura de validação de dados
+    const keys = Object.keys(req.body);   // cria um array de chaves
+
+    for (key of keys) {             // verifica em cada uma das chaves se alguma está vazia e se sim, retorna menssagem
+        if (req.body[key] == "") {
+            return res.send("Please, fill all fields!"); 
+        }       
+    }
+
+    //desestruturando objeto
+    let { avatar_url, name, birth, education, class_type, expertise } = req.body;
+
+    //tratamento dos dados 
+    birth = Date.parse(birth);    //padronizando data de nascimento para TIMESTAMP
+    const created_at = Date.now();  //adicionando data de inserção do dado 
+    const id = Number(data.teachers.length + 1);  //adicionando ID
+
+    // adicionar novos arrays ao db
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        education,
+        class_type,
+        expertise,
+        created_at
+    });
+
+    // utilizando o fs para criação de dados
+    fs.writeFile('data.json', JSON.stringify(data, null, 4), function(err) {     //data.json = caminho - JSON.stringfy = transforma objeto JS em objeto JSON - callback = se der erro de gravação retorna mensagem, senão redireciona para página
+        if (err) {
+            return res.send("Write file error!");
+        } 
+
+        return res.redirect("/teachers");
+    });
+};
+
+// ============= SHOW =============
+exports.show = function (req, res) {
+    const { id } = req.params;  //desestruturando o id do params
+
+    const foundTeacher = data.teachers.find(function(teacher) {    //procurando professor dentro do BD
+        return teacher.id == id;     // se o id do professor for igual ao id do params retorna true
+    })
+
+    if (! foundTeacher) {    //se não encontrou o professor
+        return res.send("Teacher not found!");   //retorna a menssagem
+    }
+
+    //tratando os dados
+    const teacher = {
+        ...foundTeacher,     //espalhando (spread operator = coloca dentro do objeto teacher todo o que ha dentro do objeto foundTeacher) os dados dentro da variável
+        age: age(foundTeacher.birth),    //inserindo idade com a function age
+        expertise: foundTeacher.expertise.split(","),  //.spli() transforma uma string em um array, dentro do parênteses é informado onde é a quebra, neste caso, na vírgula
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundTeacher.created_at),    //formatando timestamp para data pt-br
+    }
+
+    return res.render("teachers/show", { teacher }); //se encontrou o professor renderiza a page show enviando os dados do "teacher"
+};
+
+// ============= EDIT =============
+exports.edit = function (req, res) {
+    const { id } = req.params;  //desestruturando o id do params
+
+    const foundTeacher = data.teachers.find(function(teacher) {    //procurando professor dentro do BD
+        return teacher.id == id;     // se o id do professor for igual ao id do params retorna true
+    })
+
+    if (! foundTeacher) {    //se não encontrou o professor
+        return res.send("Teacher not found!");   //retorna a menssagem
+    }
+
+    teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth)
+    }
+
+    return res.render("teachers/edit", { teacher });
+};
---
+++ fase_03/01_controle_de_academia/desafio_04-4_show-edit-format-teacher/utils.js
@@ -0,0 +1,26 @@
+module.exports = {
+    // lógica para idade
+    age: function (timestamp) {
+        const today = new Date();    //criando um novo objeto de data com a data de hoje e colocando na variável today
+        const birthDate = new Date(timestamp);  //criando um novo objeto de data fornecendo o timestamp com o aniversário da pessoa
+    
+        let age = today.getFullYear() - birthDate.getFullYear();   //.getFullYear() retorna o ano da data especificada de acordo com a hora local.
+        const month = today.getMonth() - birthDate.getMonth();   //.getMonth() retorna o mês na data especificada de acordo com o horário local, como um valor zero-based (onde o zero indica o primeiro mês do ano).        
+    
+        if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {      //se ainda não chegou ao mês de aniversáro OU se está no mês de aniversário E hoje é menor que o aniversário
+            age = age - 1;      //diminui um ano na variável age
+        }
+    
+        return age; 
+    },
+
+    //lógica para retornar padrão de data yyyy-mm-dd
+    date: function (timestamp) {
+        const date = new Date(timestamp);
+        const year = date.getUTCFullYear();     // converte utilizando o tempo universal
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2);      //.getMonth retorna o mês de 0 a 11, então soma 1 para chegar ao mês exato
+        const day = `0${date.getUTCDate()}`.slice(-2);      //.slice() retorna uma cópia de parte de um array a partir de um subarray criado entre as posições início e fim (fim não é necessário) de um array original. O Array original não é modificado.
+
+        return `${year}-${month}-${day}`;
+    }
+}
