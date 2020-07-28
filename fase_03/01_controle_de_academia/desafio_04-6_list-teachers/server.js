61a14ca732e1cea085a8839f683cca64ea930064
Jean Monteiro <jeanmontt@hotmail.com>
2020-07-28 13:59:26 -0300

    Added 4-6 challenge

---
+++ fase_03/01_controle_de_academia/desafio_04-6_list-teachers/server.js
@@ -0,0 +1,26 @@
+//requisições
+const express = require('express');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+const methodOverride = require('method-override');
+
+const server = express();
+
+server.use(express.urlencoded({ extended: true }));     //Config recebimento de dados no body
+server.use(express.static('public'));   //Config statics
+server.use(methodOverride('_method'));  //Config method-override
+server.use(routes);     //Config routes
+
+//config template engine
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+    autoescape: false
+});
+
+//on server
+server.listen(5000, function () {
+    console.log("Server is running");
+});
